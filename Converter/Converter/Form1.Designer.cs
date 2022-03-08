57259a11be63d1282fcd65e6d03494a579224856
UzheNeKate <eavrub@gmail.com>
2022-03-08 13:23:49 +0300

    lab1 added

---
+++ Converter/Converter/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿
+namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.C_Input = new System.Windows.Forms.NumericUpDown();
+            this.K_Input = new System.Windows.Forms.NumericUpDown();
+            this.Y_Input = new System.Windows.Forms.NumericUpDown();
+            this.M_Input = new System.Windows.Forms.NumericUpDown();
+            this.B_Input = new System.Windows.Forms.NumericUpDown();
+            this.A_Input = new System.Windows.Forms.NumericUpDown();
+            this.L_Input = new System.Windows.Forms.NumericUpDown();
+            this.V_Input = new System.Windows.Forms.NumericUpDown();
+            this.S_Input = new System.Windows.Forms.NumericUpDown();
+            this.H_Input = new System.Windows.Forms.NumericUpDown();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.C_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.K_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Y_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.L_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.V_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.S_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.H_Input)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // C_Input
+            // 
+            this.C_Input.Location = new System.Drawing.Point(73, 147);
+            this.C_Input.Name = "C_Input";
+            this.C_Input.Size = new System.Drawing.Size(150, 27);
+            this.C_Input.TabIndex = 0;
+            this.C_Input.ValueChanged += new System.EventHandler(this.CMYK_ValueChanged);
+            // 
+            // K_Input
+            // 
+            this.K_Input.Location = new System.Drawing.Point(73, 246);
+            this.K_Input.Name = "K_Input";
+            this.K_Input.Size = new System.Drawing.Size(150, 27);
+            this.K_Input.TabIndex = 1;
+            this.K_Input.ValueChanged += new System.EventHandler(this.CMYK_ValueChanged);
+            // 
+            // Y_Input
+            // 
+            this.Y_Input.Location = new System.Drawing.Point(73, 213);
+            this.Y_Input.Name = "Y_Input";
+            this.Y_Input.Size = new System.Drawing.Size(150, 27);
+            this.Y_Input.TabIndex = 2;
+            this.Y_Input.ValueChanged += new System.EventHandler(this.CMYK_ValueChanged);
+            // 
+            // M_Input
+            // 
+            this.M_Input.Location = new System.Drawing.Point(73, 180);
+            this.M_Input.Name = "M_Input";
+            this.M_Input.Size = new System.Drawing.Size(150, 27);
+            this.M_Input.TabIndex = 3;
+            this.M_Input.ValueChanged += new System.EventHandler(this.CMYK_ValueChanged);
+            // 
+            // B_Input
+            // 
+            this.B_Input.Location = new System.Drawing.Point(337, 213);
+            this.B_Input.Maximum = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+            this.B_Input.Minimum = new decimal(new int[] {
+            128,
+            0,
+            0,
+            -2147483648});
+            this.B_Input.Name = "B_Input";
+            this.B_Input.Size = new System.Drawing.Size(150, 27);
+            this.B_Input.TabIndex = 4;
+            this.B_Input.ValueChanged += new System.EventHandler(this.LAB_ValueChanged);
+            // 
+            // A_Input
+            // 
+            this.A_Input.Location = new System.Drawing.Point(337, 180);
+            this.A_Input.Maximum = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+            this.A_Input.Minimum = new decimal(new int[] {
+            128,
+            0,
+            0,
+            -2147483648});
+            this.A_Input.Name = "A_Input";
+            this.A_Input.Size = new System.Drawing.Size(150, 27);
+            this.A_Input.TabIndex = 5;
+            this.A_Input.ValueChanged += new System.EventHandler(this.LAB_ValueChanged);
+            // 
+            // L_Input
+            // 
+            this.L_Input.Location = new System.Drawing.Point(337, 147);
+            this.L_Input.Name = "L_Input";
+            this.L_Input.Size = new System.Drawing.Size(150, 27);
+            this.L_Input.TabIndex = 6;
+            this.L_Input.ValueChanged += new System.EventHandler(this.LAB_ValueChanged);
+            // 
+            // V_Input
+            // 
+            this.V_Input.Location = new System.Drawing.Point(591, 213);
+            this.V_Input.Name = "V_Input";
+            this.V_Input.Size = new System.Drawing.Size(150, 27);
+            this.V_Input.TabIndex = 7;
+            this.V_Input.ValueChanged += new System.EventHandler(this.HSV_ValueChanged);
+            // 
+            // S_Input
+            // 
+            this.S_Input.Location = new System.Drawing.Point(591, 180);
+            this.S_Input.Name = "S_Input";
+            this.S_Input.Size = new System.Drawing.Size(150, 27);
+            this.S_Input.TabIndex = 8;
+            this.S_Input.ValueChanged += new System.EventHandler(this.HSV_ValueChanged);
+            // 
+            // H_Input
+            // 
+            this.H_Input.Location = new System.Drawing.Point(591, 147);
+            this.H_Input.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.H_Input.Name = "H_Input";
+            this.H_Input.Size = new System.Drawing.Size(150, 27);
+            this.H_Input.TabIndex = 9;
+            this.H_Input.ValueChanged += new System.EventHandler(this.HSV_ValueChanged);
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(620, 343);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(121, 29);
+            this.btnChoose.TabIndex = 10;
+            this.btnChoose.Text = "Choose color";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "M";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "K";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(304, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "L";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(301, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "A";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(301, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "B";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(554, 149);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "H";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(554, 182);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 20);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "S";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(553, 215);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 20);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "V";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.H_Input);
+            this.Controls.Add(this.S_Input);
+            this.Controls.Add(this.V_Input);
+            this.Controls.Add(this.L_Input);
+            this.Controls.Add(this.A_Input);
+            this.Controls.Add(this.B_Input);
+            this.Controls.Add(this.M_Input);
+            this.Controls.Add(this.Y_Input);
+            this.Controls.Add(this.K_Input);
+            this.Controls.Add(this.C_Input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.C_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.K_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Y_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.L_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.V_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.S_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.H_Input)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown C_Input;
+        private System.Windows.Forms.NumericUpDown K_Input;
+        private System.Windows.Forms.NumericUpDown Y_Input;
+        private System.Windows.Forms.NumericUpDown M_Input;
+        private System.Windows.Forms.NumericUpDown B_Input;
+        private System.Windows.Forms.NumericUpDown A_Input;
+        private System.Windows.Forms.NumericUpDown L_Input;
+        private System.Windows.Forms.NumericUpDown V_Input;
+        private System.Windows.Forms.NumericUpDown S_Input;
+        private System.Windows.Forms.NumericUpDown H_Input;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Converter/Converter/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class Form1 : Form
+    {
+        public static bool NotifyChanges = true;
+        public static (double x, double y, double z) White = (95.047, 100.0, 108.883);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoose_Click(object sender, System.EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            
+            BackColor = colorDialog.Color;
+
+            NotifyChanges = false;
+
+            var newCmyk = RGB_TO_CMYK(colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B);
+            var newXyz = RGB_TO_LAB(colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B);
+            var newHsl = RGB_TO_HSV(colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B);
+
+            SetCMYK(newCmyk);
+            SetLAB(newXyz);
+            SetHSV(newHsl);
+            
+            NotifyChanges = true;
+        }
+
+        private void SetCMYK((double c, double m, double y, double k) color)
+        {
+            var (c, m, y, k) = color;
+            if (c < 0 || c > (double)C_Input.Maximum 
+            || m < 0 || m > (double)M_Input.Maximum
+            || y < 0 || y > (double)Y_Input.Maximum
+            || k < 0 || k > (double)K_Input.Maximum)
+            {
+                MessageBox.Show("This color cannot be shown in CMYK");
+            }
+
+            C_Input.Value = Math.Max(0, Math.Min(C_Input.Maximum, (decimal) c)); 
+            M_Input.Value = Math.Max(0, Math.Min(M_Input.Maximum, (decimal) m)); 
+            Y_Input.Value = Math.Max(0, Math.Min(Y_Input.Maximum, (decimal) y)); 
+            K_Input.Value = Math.Max(0, Math.Min(K_Input.Maximum, (decimal) k));
+        }
+
+        private void SetLAB((double l, double a, double b) color)
+        {
+            var (l, a, b) = color;
+            if (l < 0 || l > (double)L_Input.Maximum
+                            || a < (double)A_Input.Minimum || a > (double)A_Input.Maximum
+                            || b < (double)B_Input.Minimum || b > (double)B_Input.Maximum)
+            {
+                MessageBox.Show("This color cannot be shown in LAB");
+            }
+
+            L_Input.Value = (int)Math.Max((double)L_Input.Minimum, Math.Min((double)L_Input.Maximum, l));
+            A_Input.Value = (int)Math.Max((double)A_Input.Minimum, Math.Min((double)A_Input.Maximum, a));
+            B_Input.Value = (int)Math.Max((double)B_Input.Minimum, Math.Min((double)B_Input.Maximum, b));
+        }
+
+        private void SetHSV((double h, double s, double v) color)
+        {
+            var (h, s, v) = color;
+            if (h < 0 || h > (double)H_Input.Maximum
+                            || s < 0 || s > (double)S_Input.Maximum
+                            || v < 0 || v > (double)V_Input.Maximum)
+            {
+                MessageBox.Show("This color cannot be shown in HSV");
+            }
+
+            H_Input.Value = Math.Max(0, Math.Min((int)H_Input.Maximum, (int)h));
+            S_Input.Value = Math.Max(0, Math.Min((int)S_Input.Maximum, (int)(s)));
+            V_Input.Value = Math.Max(0, Math.Min((int)V_Input.Maximum, (int)(v)));
+        }
+
+        private void CMYK_ValueChanged(object sender, EventArgs e)
+        {
+            if (!NotifyChanges) 
+                return;
+
+            NotifyChanges = false;
+
+            var (r, g, b) = CMYK_TO_RGB(
+                (double)C_Input.Value / 100.0,
+                (double)M_Input.Value / 100.0,
+                (double)Y_Input.Value / 100.0,
+                (double)K_Input.Value / 100.0);
+
+            try
+            {
+                BackColor = Color.FromArgb((int)r, (int)g, (int)b);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("This color cannot be shown");
+            }
+
+            SetLAB(RGB_TO_LAB(r, g, b));
+            SetHSV(RGB_TO_HSV(r, g, b));
+
+            NotifyChanges = true;
+        }
+
+        private void LAB_ValueChanged(object sender, EventArgs e)
+        {
+            if (!NotifyChanges) 
+                return;
+
+            NotifyChanges = false;
+
+            var (r, g, b) = LAB_TO_RGB(
+                (double)L_Input.Value,
+                (double)A_Input.Value,
+                (double)B_Input.Value);
+
+            try
+            {
+                BackColor = Color.FromArgb((int) r, (int) g, (int) b);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("This color cannot be shown");
+            }
+
+
+            SetCMYK(RGB_TO_CMYK(r, g, b));
+            SetHSV(RGB_TO_HSV(r, g, b));
+
+            NotifyChanges = true;
+        }
+
+        private void HSV_ValueChanged(object sender, EventArgs e)
+        {
+            if (!NotifyChanges) 
+                return;
+
+            NotifyChanges = false;
+
+            var (r, g, b) = HSV_TO_RGB(
+                (double)H_Input.Value,
+                (double)S_Input.Value,
+                (double)V_Input.Value);
+
+            try
+            {
+                BackColor = Color.FromArgb((int) r, (int) g, (int) b);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("This color cannot be shown");
+            }
+
+            SetCMYK(RGB_TO_CMYK(r, g, b));
+            SetLAB(RGB_TO_LAB(r, g, b));
+
+            NotifyChanges = true;
+        }
+
+
+
+
+        private static (double r, double g, double b) CMYK_TO_RGB(double c, double m, double y, double k)
+        {
+            var r = Math.Min(255, Math.Max(0, (255 * (1 - c) * (1 - k))));
+            var g = Math.Min(255, Math.Max(0, (255 * (1 - m) * (1 - k))));
+            var b = Math.Min(255, Math.Max(0, (255 * (1 - y) * (1 - k))));
+
+            r = Math.Round(r);
+            g = Math.Round(g);
+            b = Math.Round(b);
+
+            return (r, g, b);
+        }
+
+        private static (double c, double m, double y, double k) RGB_TO_CMYK(double r, double g, double b)
+        {
+            var k = Math.Min(Math.Min(1 - r / 255.0, 1 - g / 255.0), 1 - b / 255.0);
+
+            if (k is 1)
+            {
+                return (0, 0, 0, 100);
+            }
+
+            var c = Math.Round((1 - (r / 255.0) - k) / (1 - k) * 100);
+            var m = Math.Round((1 - (g / 255.0) - k) / (1 - k) * 100);
+            var y = Math.Round((1 - (b / 255.0) - k) / (1 - k) * 100);
+            k *= 100;
+
+            return (c, m, y, k);
+        }
+
+        private static (double l, double a, double b) RGB_TO_LAB(double r, double g, double b)
+        {
+            var rn = F(r / 255.0);
+            var gn = F(g / 255.0);
+            var bn = F(b / 255.0);
+
+            var x = 100 * (0.4125 * rn + 0.3576 * gn + 0.1804 * bn);
+            var y = 100 * (0.2127 * rn + 0.7152 * gn + 0.0722 * bn);
+            var z = 100 * (0.0193 * rn + 0.1192 * gn + 0.9502 * bn);
+
+            return (
+                Math.Round(116 * G(y / White.y) - 16),
+                Math.Round(500 * (G(x / White.x) - G(y / White.y))),
+                Math.Round(200 * (G(y / White.y) - G(z / White.z)))
+            );
+
+            double G(double x)
+            {
+                return (x >= 0.008856) ? Math.Pow(x, 1 / 3.0) : (7.787 * x + 16 / 116.0);
+            }
+
+            double F(double x)
+            {
+                return (x > 0.04045) ? (Math.Pow((x + 0.055 / 1.055), 2.4)) : (x / 12.92);
+            }
+
+        }
+
+        private static (double r, double g, double b) LAB_TO_RGB(double l, double a, double b)
+        {
+           var (x,y,z) = (
+                F(a / 500.0 + (l + 16) / 116.0) * White.x,
+                F((l + 16) / 116.0) * White.y,
+                F((l + 16) / 116.0 - b / 200.0) * White.z
+            );
+
+            var rn = 3.2406 * x / 100.0 - 1.5372 * y / 100.0 - 0.4986 * z / 100.0;
+            var gn = -0.9689 * x / 100.0 + 1.8758 * y / 100.0 + 0.0415 * z / 100.0;
+            var bn = 0.0557 * x / 100.0 - 0.2040 * y / 100.0 + 1.0570 * z / 100.0;
+
+            var r = Math.Round(Math.Min(255, Math.Max(0, Math.Round(G(rn) * 255))));
+            var g = Math.Round(Math.Min(255, Math.Max(0, Math.Round(G(gn) * 255))));
+            var B = Math.Round(Math.Min(255, Math.Max(0, Math.Round(G(bn) * 255))));
+
+            return (r, g, B);
+
+            double F(double x)
+            {
+                return Math.Pow(x, 3) >= 0.008856 ? Math.Pow(x, 3.0) : (x - 16 / 116.0) / 7.787;
+            }
+
+            double G(double x)
+            {
+                return (x >= 0.0031308) ? (1.055 * Math.Pow(x, 1 / 2.4) - 0.055) : 12.92 * x;
+            }
+        }
+
+        private static (double h, double s, double v) RGB_TO_HSV(double r, double g, double b)
+        {
+            r /= 255.0;
+            g /= 255.0;
+            b /= 255.0;
+
+            var min = Math.Min(Math.Min(r, g), b);
+            var max = Math.Max(Math.Max(r, g), b);
+            var v = max;
+            var delta = max - min;
+            double s, h;
+            if (max != 0)
+                s = delta / max;
+            else
+            {
+                s = 0;
+                h = 0;
+                return (h, s, v);
+            }
+            if (r == max)
+                h = (g - b) / delta;
+            else if (g == max)
+                h = 2 + (b - r) / delta;
+            else
+                h = 4 + (r - g) / delta;
+            h *= 60;
+
+            if (h < 0)
+                h += 360;
+
+            h = Math.Round(h);
+            s = Math.Round(s * 100);
+            v = Math.Round(v * 100);
+
+            return (h, s, v);
+        }
+
+        private static (double r, double g, double b) HSV_TO_RGB(double h, double s, double v)
+        {
+            int i;
+            double f, m, n, k;
+            double r, g, b;
+            s /= 100.0;
+            v /= 100.0;
+            if (s == 0)
+            {
+                r = g = b = v;
+            }
+            else
+            {
+                h /= 60;
+                i = (int)Math.Floor(h);
+                f = h - i;
+                m = v * (1 - s);
+                n = v * (1 - s * f);
+                k = v * (1 - s * (1 - f));
+
+                switch (i)
+                {
+                    case 0: {
+                        r = v; g = k; b = m;
+                        break;
+                    }
+                    case 1: {
+                        r = n; g = v; b = m;
+                        break;
+                    }
+                    case 2: {
+                        r = m; g = v; b = k;
+                        break;
+                    }
+                    case 3: {
+                        r = m; g = n; b = v;
+                        break;
+                    }
+                    case 4: {
+                        r = k; g = m; b = v;
+                        break;
+                    }
+                    default: {
+                        r = v; g = m; b = n;
+                        break;
+                    }
+                }
+            }
+
+            r = Math.Round(r * 255);
+            g = Math.Round(g * 255);
+            b = Math.Round(b * 255);
+
+            return (r, g, b);
+        }
+    }
+}
